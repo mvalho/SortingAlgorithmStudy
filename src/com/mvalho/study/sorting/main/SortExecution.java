847b16c496d9191585687a8cda88a081ccbd73c6
Marcelo Carvalho <Marcelo Carvalho>
2016-05-14 13:19:36 -0300

    Fix typo. Add comments to the main class SortExecution

--- src/com/mvalho/study/sorting/main/SortExecution.java
+++ src/com/mvalho/study/sorting/main/SortExecution.java
@@ -8,10 +8,16 @@ public class SortExecution {
 		int[] arrayToSort = {8,7,2,0,1,4,3,5,9};
 		System.out.println("Array to be sorted out: " + printArray(arrayToSort));
 		
+		/*
+		 * BUBBLE SORT - IMPLEMANTATION
+		 */
 		BubbleSortOperation bubbleSort = new BubbleSortOperation();
 		System.out.println( "Array sorted by Bubble Sort: " + printArray( bubbleSort.sortArray( arrayToSort ) ) );
 	}
 	
+	/*
+	 * Prints the array values in a simple way.
+	 */
 	private static String printArray(int[] array) {
 		StringBuilder stringBuilder = new StringBuilder();
 		for (int i : array) {
